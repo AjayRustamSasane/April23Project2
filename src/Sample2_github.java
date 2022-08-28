bee6acf43d479c994bbd6a636acf3822fdc0e2f3
Lenono <Lenono@DESKTOP-3HKEH2P>
2022-08-28 20:02:40 +0530

    [Ajay] Project 2

---
+++ src/Sample2_github.java
@@ -0,0 +1,16 @@
+
+public class Sample2_github 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Good Moring Ajay");
+		System.out.println("Good Moring Ajay");
+		System.out.println("Good Moring Ajay");
+		System.out.println("Good Moring Ajay");
+		System.out.println("Good Moring Ajay");
+		System.out.println("Good Moring Ajay");
+		System.out.println("Good Moring Ajay");
+
+		
+	}
+}
---
+++ src/sample1_github.java
@@ -0,0 +1,11 @@
+
+public class sample1_github 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Github project 2");
+		
+		System.out.println("New project 2");
+		
+	}
+}
